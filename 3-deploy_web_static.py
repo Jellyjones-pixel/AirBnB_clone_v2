43deaf7fa419fc8c0e1ce847d4e790f8a7c32bd1
jellyjones-pixel <johnmbithimutave@gmail.com>
2024-04-10 19:23:10 +0300

    Updated

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['100.25.205.186', '100.25.142.236']
+env.hosts = ['34.227.90.9', '107.21.38.138']
 
 
 def do_pack():
